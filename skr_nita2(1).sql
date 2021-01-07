48b12e47a8190f26f91742bee97f90a3dbb1591d
git-jendro <jendro.hard@gmail.com>
2021-01-07 19:51:59 +0700

    sql

--- skr_nita2(1).sql
+++ skr_nita2(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2021 at 01:47 PM
+-- Generation Time: Jan 07, 2021 at 01:51 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
